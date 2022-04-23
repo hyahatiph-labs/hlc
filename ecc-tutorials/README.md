8450d9a17aeb90b368dc7a6095f43a88452ed0f8
reemuru <rimuru@hiahatf.org>
2022-04-22 19:29:05 -0500

    ecc-tutorials: initial commit

---
+++ ecc-tutorials/README.md
@@ -0,0 +1,19 @@
+# Elliptic curve cryptography (ECC) beginner tutorials in Typescript
+
+## Jupyter Notebook
+
+Typescript and Jupyter Notebook? Yes! With [tslab](https://github.com/yunabe/tslab)!
+
+## Instructions
+
+> Just download the repo and read the tutorial `.ipynb` files!
+> Notes:
+
+    These tutorials are made with Monero (XMR) cryptocurrency in mind, but not too much. The ubiquity of ECC means that these tutorials must also be applicable to other cryptocurrencies.
+    These tutorials only cover working with ECC in general. Things specific to the ed25519 curve (the curve used here) like elliptic curve description, cofactor consideration, etc. are not included.
+    For educational purposes only!
+
+Ported from the link below for research
+
+Reference:
+https://github.com/coinstudent2048/ecc_tutorials
---
+++ ecc-tutorials/src/util.ts
@@ -0,0 +1,47 @@
+import * as ed25519 from '@noble/ed25519';
+
+/**
+ * Convert BigInteger to byte array
+ * @param {bigint} n - bigint value
+ * @returns {Promise<number[]>} byte array
+ */
+export const big_int_to_byte_array = async (n: bigint): Promise<number[]> => {
+    // we want to represent the input as a 32-byte array
+    const a: number[] = [0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < a.length; ++i) {
+        const b = n & BigInt(0xff);
+        a [i] = Number(b);
+        n = (n - b) / BigInt(256) ;
+    }
+    return a;
+};
+
+/**
+ * Convert Uint8Array to BigInteger
+ * @param {Uint8Array} a - byte array
+ * @returns {Promise<bigint>} big integer converted from array
+ */
+export const byte_array_to_big_int = async (a: Uint8Array): Promise<bigint> => {
+    let value = BigInt(0);
+    for (let i = a.length - 1; i >= 0; --i) {
+        value = value * BigInt(256) + BigInt(a[i]);
+    }
+    return value;
+};
+
+/**
+ * Make a valid scalar
+ * @param {Uint8Array} v - bytes to reduce
+ * @returns {Promise<string>} hex string reduced mod l
+ */
+export const sc_reduce_32 = async (v: Uint8Array): Promise<string> => {
+    const m = ed25519.utils.mod(
+        await byte_array_to_big_int(v), ed25519.CURVE.l
+    );
+    return ed25519.utils.bytesToHex(
+        new Uint8Array(await big_int_to_byte_array(m))
+    );
+}
